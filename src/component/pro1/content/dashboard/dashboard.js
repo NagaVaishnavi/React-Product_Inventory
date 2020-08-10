cd8b888968bd6ea83c1035ea0a7aef540106eeb2
NagaVaishnavi Gajula <vaishu.g738@gmail.com>
2020-08-10 10:46:02 +0530

    changed

--- src/component/pro1/content/dashboard/dashboard.js
+++ src/component/pro1/content/dashboard/dashboard.js
@@ -33,27 +33,7 @@ class  Dashboard extends React.Component {
     legendToggle
   />
 
-<Chart
-    width={500}
-    height={'700px'}
-    chartType="AreaChart"
-    loader={<div>Loading Chart</div>}
-    data={[
-      ['Year', 'Sales', 'Expenses'],
-      ['2013', 1000, 400],
-      ['2014', 1170, 460],
-      ['2015', 660, 1120],
-      ['2016', 1030, 540],
-    ]}
-    options={{
-      title: 'Company Performance',
-      hAxis: { title: 'Year', titleTextStyle: { color: '#333' } },
-      vAxis: { minValue: 0 },
-      // For the legend to fit, we make the chart area smaller
-      chartArea: { width: '50%', height: '70%' },
-      // lineWidth: 25
-    }}
-  />
+
 <Chart
   width={'600px'}
   height={'300px'}
